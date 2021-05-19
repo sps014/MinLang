491119dbd55da2e3002e813faddba847690456ea
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-05-19 21:15:22 +0530

    added bitwise and or support

--- src/lang/code_analysis/syntax_kind.rs
+++ src/lang/code_analysis/syntax_kind.rs
@@ -10,8 +10,12 @@ pub enum SyntaxKind {
     MinusToken = 51,
     SlashToken = 52,
     StarToken = 53,
-
-
+    BangToken = 54,
+    EqualEqualToken=55,
+    AmpersandAmpersandToken=56,
+    PipePipeToken=57,
+    BitWiseOrToken=58,
+    BitWiseAndToken=59,
 
     OpenParenthesisToken = 70,
     CloseParenthesisToken = 71,
@@ -19,6 +23,40 @@ pub enum SyntaxKind {
     NumberExpressionToken=100,
     BinaryExpressionToken=101,
     ParenthesizedExpressionToken=102,
-    
+
     BadToken = 1000,
+}
+impl SyntaxKind
+{
+    pub fn get_binary_precedence(&self)->i32
+    {
+        return  match self
+        {
+            SyntaxKind::BitWiseAndToken=>9,
+            SyntaxKind::BitWiseOrToken=>8,
+            SyntaxKind::SlashToken =>5,
+            SyntaxKind::StarToken => 5,
+
+            SyntaxKind::PlusToken=>4,
+            SyntaxKind::MinusToken=>4,
+
+            SyntaxKind::EqualEqualToken=>3,
+            SyntaxKind::BangToken=>3,
+
+            SyntaxKind::AmpersandAmpersandToken=>2,
+            SyntaxKind::PipePipeToken=>1,
+
+            _=>0
+        }
+    }
+    pub  fn get_unary_precedence(&self)->i32
+    {
+       return match self
+       {
+           SyntaxKind::PlusToken=>6,
+           SyntaxKind::MinusToken=>6,
+           SyntaxKind::BangToken=>6,
+           _=>0
+       }
+    }
 }
