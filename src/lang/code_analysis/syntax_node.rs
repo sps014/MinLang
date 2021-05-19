6e2879b81033fb93f1e56bb07410caffcab778a9
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-05-19 12:17:34 +0530

    added children in tree

--- src/lang/code_analysis/syntax_node.rs
+++ src/lang/code_analysis/syntax_node.rs
@@ -1,7 +1,3 @@
-use std::any::Any;
-
-use crate::lang::code_analysis::syntax_kind::SyntaxKind;
-
 use super::syntax_token::SyntaxToken;
 
 #[derive(Debug)]
@@ -9,4 +5,33 @@ pub enum SyntaxNode {
     NumberExpressionSyntax(SyntaxToken),
     BinaryExpressionSyntax(Box<SyntaxNode>, SyntaxToken, Box<SyntaxNode>),
     ParenthesizedExpressionSyntax(SyntaxToken, Box<SyntaxNode>, SyntaxToken),
+}
+
+impl SyntaxNode {
+    pub fn get_children(&self) -> Vec<SyntaxToken> {
+        return match self {
+            SyntaxNode::NumberExpressionSyntax(n) => vec![n.clone()],
+            SyntaxNode::BinaryExpressionSyntax(left, opr, right) => {
+                let mut v = vec![];
+                for i in left.get_children() {
+                    v.push(i);
+                }
+                v.push(opr.clone());
+                for i in right.get_children() {
+                    v.push(i);
+                }
+                v
+            }
+            SyntaxNode::ParenthesizedExpressionSyntax(open, expr, close) => {
+                let mut v = vec![];
+                v.push(open.clone());
+                for i in expr.get_children() {
+                    v.push(i);
+                }
+                v.push(close.clone());
+                v
+            }
+            _ => vec![],
+        };
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod lang;
 use lang::code_analysis::parser::*;
 use crate::lang::code_analysis::evaluator::Evaluator;
-use std::error::Error;
 
 fn main() {
     call();
 }
 fn call() {
-    let input = "1+2*6/+3";
+    let input = "(1+2*(6/2)+3)";
     let mut p = Parser::new(input);
     let tree=p.parse();
     let e=Evaluator::new(*tree.root);
