28159bdd1c6d7a2479cd91dcc13f485bdec0f288
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-22 17:07:47 +0530

    removed old impl

--- src/lang/code_analysis/syntax_node.rs
+++ src/lang/code_analysis/syntax_node.rs
@@ -1,44 +1,43 @@
 use std::vec;
 
-use super::syntax_token::SyntaxToken;
 
-#[derive(Debug, Clone)]
-pub enum SyntaxNode {
-    StatementsSyntax(Vec<Box<SyntaxNode>>),
-    NumberExpressionSyntax(SyntaxToken),
-    BinaryExpressionSyntax(Box<SyntaxNode>, SyntaxToken, Box<SyntaxNode>),
-    ParenthesizedExpressionSyntax(SyntaxToken, Box<SyntaxNode>, SyntaxToken),
-    UnaryExpressionSyntax(SyntaxToken, Box<SyntaxNode>),
-    AssignmentExpressionSyntax(SyntaxToken, SyntaxToken, Box<SyntaxNode>),
-    FunctionCallExpression(SyntaxToken, SyntaxToken, Vec<Box<SyntaxNode>>, SyntaxToken),
-    BlockExpressionSyntax(SyntaxToken, Vec<Box<SyntaxNode>>, SyntaxToken),
-    WhileLoopSyntax(SyntaxToken, Box<SyntaxNode>, Box<SyntaxNode>),
-    IfBlockSyntax(SyntaxToken, Box<SyntaxNode>,Box<SyntaxNode>),
-}
-enum SyntaxCol {
-    Token(SyntaxToken),
-    Node(SyntaxNode),
-}
-
-impl SyntaxNode {
-    pub fn get_children(&self) -> Vec<SyntaxCol> {
-        return match self {
-            SyntaxNode::NumberExpressionSyntax(n) => vec![SyntaxCol::Token(n.clone())],
-            SyntaxNode::BinaryExpressionSyntax(left, opr, right) => {
-                vec![
-                    SyntaxCol::Node(left.as_ref().clone()),
-                    SyntaxCol::Token(opr.clone()),
-                    SyntaxCol::Node(right.as_ref().clone()),
-                ]
-            }
-            SyntaxNode::ParenthesizedExpressionSyntax(open, expr, close) => {
-                vec![
-                    SyntaxCol::Token(open.clone()),
-                    SyntaxCol::Node(expr.as_ref().clone()),
-                    SyntaxCol::Token(close.clone()),
-                ]
-            }
-            _ => vec![],
-        };
-    }
-}
+// #[derive(Debug, Clone)]
+// pub enum SyntaxNode {
+//     StatementsSyntax(Vec<Box<SyntaxNode>>),
+//     NumberExpressionSyntax(SyntaxToken),
+//     BinaryExpressionSyntax(Box<SyntaxNode>, SyntaxToken, Box<SyntaxNode>),
+//     ParenthesizedExpressionSyntax(SyntaxToken, Box<SyntaxNode>, SyntaxToken),
+//     UnaryExpressionSyntax(SyntaxToken, Box<SyntaxNode>),
+//     AssignmentExpressionSyntax(SyntaxToken, SyntaxToken, Box<SyntaxNode>),
+//     FunctionCallExpression(SyntaxToken, SyntaxToken, Vec<Box<SyntaxNode>>, SyntaxToken),
+//     BlockExpressionSyntax(SyntaxToken, Vec<Box<SyntaxNode>>, SyntaxToken),
+//     WhileLoopSyntax(SyntaxToken, Box<SyntaxNode>, Box<SyntaxNode>),
+//     IfBlockSyntax(SyntaxToken, Box<SyntaxNode>,Box<SyntaxNode>),
+// }
+// enum SyntaxCol {
+//     Token(SyntaxToken),
+//     Node(SyntaxNode),
+// }
+//
+// impl SyntaxNode {
+//     pub fn get_children(&self) -> Vec<SyntaxCol> {
+//         return match self {
+//             SyntaxNode::NumberExpressionSyntax(n) => vec![SyntaxCol::Token(n.clone())],
+//             SyntaxNode::BinaryExpressionSyntax(left, opr, right) => {
+//                 vec![
+//                     SyntaxCol::Node(left.as_ref().clone()),
+//                     SyntaxCol::Token(opr.clone()),
+//                     SyntaxCol::Node(right.as_ref().clone()),
+//                 ]
+//             }
+//             SyntaxNode::ParenthesizedExpressionSyntax(open, expr, close) => {
+//                 vec![
+//                     SyntaxCol::Token(open.clone()),
+//                     SyntaxCol::Node(expr.as_ref().clone()),
+//                     SyntaxCol::Token(close.clone()),
+//                 ]
+//             }
+//             _ => vec![],
+//         };
+//     }
+// }
