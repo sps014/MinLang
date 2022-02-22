9fb841f8e03c0513d851afd603138a781aad527f
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-22 18:28:48 +0530

    created a dictionary

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use crate::lang::code_analysis::lexer::Lexer;
 fn main() {
 
     let mut lexer= Lexer::new("{ } + 56 . ,".to_string());
-    println!("{:?}", lexer.lex_all());
+    let tokens=lexer.lex_all();
+    for token in tokens {
+        println!("{:?}",token);
+    }
 }
 
