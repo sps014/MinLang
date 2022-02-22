7ef01f06e3829cc7c5770ecc559dfb46aa1cb52d
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-22 17:34:44 +0530

    basic lexer

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use crate::lang::code_analysis::lexer::Lexer;
 
 fn main() {
 
-    let mut lexer= Lexer::new("[ ] + 56 . ,".to_string());
+    let mut lexer= Lexer::new("{ } + 56 . ,".to_string());
     println!("{:?}", lexer.lex_all());
 }
 
