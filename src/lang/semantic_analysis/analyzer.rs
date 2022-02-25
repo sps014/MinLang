3e88ea07ba5654141d9f50cec19d8fb3d26f69bd
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-25 23:07:19 +0530

    else conditions and better error line numbers

--- src/lang/semantic_analysis/analyzer.rs
+++ src/lang/semantic_analysis/analyzer.rs
@@ -1,8 +1,8 @@
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::io::{Error, ErrorKind};
 use std::rc::Rc;
 use crate::lang::code_analysis::syntax::syntax_node::{ExpressionNode, FunctionNode, TypeLiteral, ProgramNode, StatementNode};
+use crate::lang::code_analysis::text::text_span::TextSpan;
 use crate::lang::code_analysis::token::syntax_token::SyntaxToken;
 use crate::lang::semantic_analysis::symbol_table::SymbolTable;
 use crate::Parser;
@@ -38,10 +38,11 @@ impl<'a> Anaylzer<'a> {
             Some(t) => Some(Rc::clone(t)),
             None => None,
         };
-        let mut symbol_table = Rc::new(RefCell::new(SymbolTable::new(parent_scope)));
+        let symbol_table = Rc::new(RefCell::new(SymbolTable::new(parent_scope)));
         for statement in body.iter() {
             self.analyze_statement(statement,parent_function,&symbol_table)?;
         }
+        dbg!(&symbol_table.borrow());
         Ok(())
     }
     fn analyze_statement(&self,statement:&StatementNode,parent_function:&FunctionNode,symbol_table:&Rc<RefCell<SymbolTable>>)->Result<(),Error>
@@ -70,9 +71,9 @@ impl<'a> Anaylzer<'a> {
     }
     fn analyze_assignment(&self,left:&SyntaxToken,right:&ExpressionNode,parent_function:&FunctionNode,
                           symbol_table:&Rc<RefCell<SymbolTable>>)->Result<(),Error> {
-        let right=self.analyze_expression(right,parent_function,symbol_table)?;
-        let left =(*symbol_table).as_ref().borrow().get_symbol(left.text.clone())?;
-        self.compare_data_type(&left,&right)?;
+        let r=self.analyze_expression(right,parent_function,symbol_table)?;
+        let l =(*symbol_table).as_ref().borrow().get_symbol(left.clone())?;
+        self.compare_data_type(&l,&r,&left.position)?;
         Ok(())
     }
     fn analyze_expression(&self,expression:&ExpressionNode,parent_function:&FunctionNode,
@@ -83,31 +84,31 @@ impl<'a> Anaylzer<'a> {
                 Ok(number.clone()),
             ExpressionNode::Unary(_,right)=>
                 Ok(self.analyze_expression(right,parent_function,symbol_table)?),
-            ExpressionNode::Binary(left,_,right)=>
-                Ok(self.analyze_binary_expression(left,right,parent_function,symbol_table)?),
+            ExpressionNode::Binary(left,opr,right)=>
+                Ok(self.analyze_binary_expression(left,opr,right,parent_function,symbol_table)?),
             ExpressionNode::Identifier(id)=>
                 Ok(self.analyze_identifier(id,symbol_table)?),
             _=>return Err(Error::new(ErrorKind::Other,format!("Not implemented expression {:?}",expression)))
         };
     }
-    fn analyze_binary_expression(&self,left:&ExpressionNode,right:&ExpressionNode,parent_function:&FunctionNode,
+    fn analyze_binary_expression(&self,left:&ExpressionNode,opr:&SyntaxToken,right:&ExpressionNode,parent_function:&FunctionNode,
                                  symbol_table:&Rc<RefCell<SymbolTable>>)->Result<TypeLiteral,Error> {
         let left_value = self.analyze_expression(left,parent_function,symbol_table)?;
         let right_value = self.analyze_expression(right,parent_function,symbol_table)?;
-        self.compare_data_type(&left_value,&right_value)?;
+        self.compare_data_type(&left_value,&right_value,&opr.position)?;
         return Ok(left_value);
     }
-    fn compare_data_type(&self,left:&TypeLiteral,right:&TypeLiteral)->Result<(),Error> {
+    fn compare_data_type(&self,left:&TypeLiteral,right:&TypeLiteral,position:&TextSpan)->Result<(),Error> {
         if left.get_type()==right.get_type()
         {
             return Ok(())
         }
         Err(Error::new(ErrorKind::Other,
-                       format!("Binary expression {} and {} are not same type at {} and {}",
-                       left.get_type(),right.get_type(),left.get_line_str(),right.get_line_str())))
+                       format!("cannot convert from {} to {} at {}",
+                       left.get_type(),right.get_type(),position.get_point_str())))
     }
     fn analyze_identifier(&self,id:&SyntaxToken,symbol_table:&Rc<RefCell<SymbolTable>>)->Result<TypeLiteral,Error> {
-        let r=(*symbol_table).as_ref().borrow_mut().get_symbol(id.text.clone())?;
+        let r=(*symbol_table).as_ref().borrow_mut().get_symbol(id.clone())?;
         Ok(r)
     }
 
@@ -117,7 +118,22 @@ impl<'a> Anaylzer<'a> {
                        parent_function:&FunctionNode, symbol_table:&Rc<RefCell<SymbolTable>>) ->
     Result<(),Error>
     {
-        let r=self.analyze_body(if_body,parent_function,Some(symbol_table))?;
+        //if condition
+        self.analyze_expression(condition,parent_function,symbol_table)?;
+        //if body
+        self.analyze_body(if_body,parent_function,Some(symbol_table))?;
+
+        //else if block
+        for i in else_if.iter()
+        {
+            self.analyze_expression(&i.0,parent_function,symbol_table)?;
+            self.analyze_body(&i.1,parent_function,Some(symbol_table))?;
+        }
+        match else_body
+        {
+            Some(body)=>self.analyze_body(body,parent_function,Some(symbol_table))?,
+            None=>()
+        }
         Ok(())
     }
 
