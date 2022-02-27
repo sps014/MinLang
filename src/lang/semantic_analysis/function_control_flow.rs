b0110d3637f068c555b465c510e32db0b95ad74b
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2022-02-27 15:59:04 +0530

    cleanup of control flow

--- src/lang/semantic_analysis/function_control_flow.rs
+++ src/lang/semantic_analysis/function_control_flow.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::io::{Error, ErrorKind};
 use std::rc::Rc;
@@ -32,6 +31,7 @@ impl FlowNode
             name,
         }
     }
+    #[allow(dead_code)]
     pub fn print(&self,indent: usize)
     {
         for _ in 0..indent
@@ -64,7 +64,9 @@ impl FunctionControlGraph
     pub fn build(&mut self)->Result<(),Error>
     {
         self.create_graph()?;
-        (*self.root_node.as_ref().unwrap()).as_ref().borrow().print(0);
+        //(*self.root_node.as_ref().unwrap()).as_ref().borrow().print(0);
+
+        // do not check for non void as it is checked in the analyzer already
         match self.function.return_type {
             Some(_)=>self.check_non_void_return()?,
             None=> {  },
@@ -87,29 +89,35 @@ impl FunctionControlGraph
     fn dfs(&mut self,node:&Rc<RefCell<FlowNode>>)->bool
     {
         let new=&node.as_ref().borrow();
-        let mut ct_all_true=0;
+        let mut ct_all_true=0; //keep count of no of true paths
+
         for i in (0..new.child_nodes.len()).rev()
         {
-            let child=self.dfs(&new.child_nodes[i]);
-            if child
+            //check if sub nodes of child are true are not
+            let is_child_true =self.dfs(&new.child_nodes[i]);
+
+            if is_child_true
             {
-                ct_all_true+=1;
+                ct_all_true+=1; //child path is true
             }
-            if child && new.child_nodes[i].as_ref().borrow().has_return
+            //if right most node is return then we can directly say all left sub nodes will be true
+            if is_child_true && new.child_nodes[i].as_ref().borrow().has_return
             {
                 return true;
             }
         }
-        //println!("{}: {}",new.name,ct_all_true);
 
+        //if all children are true then this node is true
         if new.child_nodes.len()!=0 && ct_all_true==new.child_nodes.len()
         {
             return true;
         }
+        //if the node is a return node then it is true
         else if new.has_return
         {
             return true;
         }
+
         false
     }
     fn create_graph(&mut self)->Result<(),Error>
@@ -118,15 +126,17 @@ impl FunctionControlGraph
         self.visit_block(&self.function.body.clone(), &self.root_node.clone().unwrap())?;
         Ok(())
     }
-    fn visit_block(&mut self, nodes:&Vec<StatementNode>, parent:&Rc<RefCell<FlowNode>>) ->Result<Rc<RefCell<FlowNode>>,Error>
+    //visit a block and pass parent accordingly
+    fn visit_block(&mut self, nodes:&Vec<StatementNode>, parent:&Rc<RefCell<FlowNode>>) ->Result<(),Error>
     {
         let mut node = parent.clone();
         for i in nodes.iter()
         {
             self.visit_node(i,&mut node)?;
         }
-        Ok(node)
+        Ok(())
     }
+    // only two statements have impact on control path return and branches and we can ignore the rest of  the branches
     fn visit_node(&mut self, statement:&StatementNode, parent:&Rc<RefCell<FlowNode>>) ->Result<(),Error>
     {
           match statement {
@@ -146,22 +156,29 @@ impl FunctionControlGraph
     {
         //if body
         let mut if_body_node = Rc::new(RefCell::new(FlowNode::new("if".to_string())));
+        // add current to parent
         (*parent).as_ref().borrow_mut().child_nodes.push(if_body_node.clone());
+        //visit it's body for sub nodes
         self.visit_block(if_body, &mut if_body_node)?;
 
+        //check same for else if blocks
         for i in else_if.iter()
         {
             if_body_node = Rc::new(RefCell::new(FlowNode::new("else if".to_string())));
+            //add to parent
             (*parent).as_ref().borrow_mut().child_nodes.push(if_body_node.clone());
+            //visit it's body for sub nodes
             self.visit_block(&i.1, &mut if_body_node)?;
         }
         match else_body {
+            //if we have else body add it to the graph
             Some(else_body)=>
             {
                 if_body_node = Rc::new(RefCell::new(FlowNode::new("else".to_string())));
                 (*parent).as_ref().borrow_mut().child_nodes.push(if_body_node.clone());
                 self.visit_block(else_body, &mut if_body_node)?;
             },
+            //if we dont have else body then we need to add an artificial body less else block
             None=>
                 {
                     if_body_node = Rc::new(RefCell::new(FlowNode::new("else".to_string())));
@@ -171,9 +188,11 @@ impl FunctionControlGraph
 
         Ok(())
     }
+
+    //add return node to parent block and mark: has return
     fn visit_return(&mut self,return_node:&ExpressionNode,parent:&Rc<RefCell<FlowNode>>)->Result<(),Error>
     {
-        let mut return_flow = Rc::new(RefCell::new(FlowNode::from(true,format!("return {:?}",return_node))));
+        let return_flow = Rc::new(RefCell::new(FlowNode::from(true,format!("return {:?}",return_node))));
         (*parent).as_ref().borrow_mut().child_nodes.push(return_flow.clone());
         Ok(())
     }
